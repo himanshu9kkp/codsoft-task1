087fbc6c0ad92ee89e23bfb69d98a18483747af4
himanshu9kkp <148487787+himanshu9kkp@users.noreply.github.com>
2023-10-20 00:01:25 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,26 @@
+let input=document.querySelector("#text");
+console.log(input);
+let btn=document.querySelectorAll("button");
+console.log(btn)
+let string='';
+let arr=Array.from(btn);
+arr.forEach(button=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML =='='){
+            string=eval(string);
+            input.value=string;
+        }else if(e.target.innerHTML=='AC'){
+            string='';
+            input.value=string;
+        }else if(e.target.innerHTML=='DA'){
+            string=string.substring(0,string.length-1);
+            input.value=string;
+
+        }
+        else{
+        string+=e.target.innerHTML;
+        input.value=string
+        }
+
+    })
+})
